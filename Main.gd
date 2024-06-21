8e1d720e25da007c9e93327a6d546facd9ca5f1b
JacobMundy <jmundy2003@gmail.com>
2024-06-21 02:19:36 -0400

    nodes resize with window
    this happens through a connector and the _on_window_resized method

--- Main.gd
+++ Main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var tab_container = $TabContainer
 @onready var add_tab_button = $AddTabButton 
 @onready var line_counter = $LineCounter
+@onready var root = get_tree().root
 
 var tab_number = 1
 
@@ -12,6 +13,15 @@ signal request_close_tab
 func _ready():
 	# Connect the button's signal to a method
 	add_tab_button.connect("pressed", Callable(self, "_on_AddTabButton_pressed"))
+	root.connect("size_changed", Callable(self,"_on_window_resized"))
+
+
+func _on_window_resized():
+	var tab_container_size = Vector2(root.size.x - 40, root.size.y)
+	var line_counter_size = Vector2(40, root.size.y)
+	tab_container.size = tab_container_size
+	line_counter.size = line_counter_size
+	
 
 func _on_AddTabButton_pressed():
 	# Logic to add a new tab to the TabContainer
