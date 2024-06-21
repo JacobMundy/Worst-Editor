a20280635f2bed8192438eb2aedb867095bc2d0a
JacobMundy <jmundy2003@gmail.com>
2024-06-21 16:48:09 -0400

    refactored
    no longer uses regex (thank god)
    default len and split/replace are much faster for a large amount of text (editor can run smoothly with 2mil char and 41k lines)
    also utilizes timer so that expensive calls happen less often

--- WordCounter.gd
+++ WordCounter.gd
@@ -3,8 +3,7 @@ extends RichTextLabel
 @onready var default_tab = get_node("../TabContainer/New 1")
 var text_edit
 var connected_editors = []
-var regex_word_count = RegEx.new()
-var regex_character_count = RegEx.new()
+var update_timer: Timer
 
 @onready var label_scroll_container = get_parent()
 
@@ -13,35 +12,47 @@ var regex_character_count = RegEx.new()
 func _ready():
 	set_new_text_edit(default_tab)
 	self.scroll_active = false
-	regex_word_count.compile("[\\w-]+")
-	regex_character_count.compile(".")
+	update_timer = Timer.new()
+	update_timer.one_shot = true
+	update_timer.timeout.connect(update_word_counter)
+	add_child(update_timer)
 
-
+func schedule_update():
+	if !update_timer.is_stopped():
+		update_timer.stop()
+	update_timer.start(0.5)  # 0.5 second delay
+	
 func set_new_text_edit(provided_text_edit):
 	text_edit = provided_text_edit
 	if text_edit not in connected_editors:
-		text_edit.connect("caret_changed", Callable(self, "update_word_counter"))
+		text_edit.connect("caret_changed", Callable(self, "on_text_changed"))
+		
 		connected_editors.append(text_edit)
 	update_word_counter()
-
+	
+# Connect this to your CodeEdit's "text_changed" signal
+func on_text_changed():
+	schedule_update()
+	
 func update_word_counter():
-	if !text_edit or !regex_word_count.is_valid() or !regex_character_count.is_valid():
-		print("Invalid text_edit or regex")
-		return
 
-	var text = text_edit.text if text_edit else ""
+	var text = text_edit.text
 	var word_count = 0
 	var character_count = 0
 
 	if text != "":
-		var char_matches = regex_character_count.search_all(text)
-		var word_matches = regex_word_count.search_all(text)
+		word_count = count_words(text)
+		character_count = count_characters(text)
 
-		if char_matches != null and word_matches != null:
-			word_count = word_matches.size()
-			character_count = char_matches.size()
-
-	# Use string formatting to ensure consistent width
-	var formatted_text = "Word Count: %7d | Character Count: %9d" % [word_count, character_count]
+	var formatted_text = "Word Count: %7d   | Character Count: %7d" % [word_count, character_count]
 	set_text(formatted_text)
 
+
+
+func count_words(text):
+	# Split the text by whitespace and filter out empty strings
+	return len(text.split(' ', true))
+
+func count_characters(text):
+	# Remove all whitespace to get the count of non-whitespace characters
+	return len(text.replace(' ', ''))
