e435298ab57af99db3571343e31291df3f5f6a70
JacobMundy <jmundy2003@gmail.com>
2024-06-21 02:18:29 -0400

    refactored
    node changed to RichTextLabel, a change for line separation was found
    _process function removed, a better way was found
    modulate removed, a better way was found
    scroll_active = better modulate
    theme constant, line separation = working line spacing
    the v_scroll_bars value_changed = more efficient than _process
    and finally scroll_to_line = a way to use the scroll_vertical value so that the rich label actually scrolls and scrolls to the right place
    
    (it is 2 am this commit message is a tragedy im sorry it MIGHT be git amended)

--- LineCounter.gd
+++ LineCounter.gd
@@ -1,4 +1,4 @@
-extends TextEdit
+extends RichTextLabel
 
 @onready var default_tab = get_node("../TabContainer/New 1")
 var text_edit
@@ -11,34 +11,28 @@ var last_text_edit_scroll = -1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_new_text_edit(default_tab)
-	self.add_theme_constant_override("line_spacing", 4)
-	self.get_v_scroll_bar().modulate = 0
+	self.scroll_active = false
+	self.add_theme_constant_override("line_separation", 4)
 
-# 
-func _process(_delta):
-	if text_edit != null:
-		# Sync the scroll position of LineCounter with text_edit
-		if text_edit.scroll_vertical != last_text_edit_scroll:
-			self.scroll_vertical = text_edit.scroll_vertical
-			last_text_edit_scroll = text_edit.scroll_vertical
-
-		# Sync the scroll position of text_edit with LineCounter
-		if self.scroll_vertical != last_self_scroll:
-			text_edit.scroll_vertical = self.scroll_vertical
-			last_self_scroll = self.scroll_vertical
 
 func set_new_text_edit(provided_text_edit):
 	text_edit = provided_text_edit
 	if text_edit not in connected_editors:
+		var scroll_bar = text_edit.get_v_scroll_bar()
 		text_edit.connect("caret_changed", Callable(self, "update_line_count"))
+		scroll_bar.connect("value_changed", Callable(self, "update_line_count"))
+		print(scroll_bar)
 		connected_editors.append(text_edit)
 	update_line_count()
 
-func update_line_count():
+
+func update_line_count(_value = -1):
 	var string_list = ""
 	var line_count = text_edit.get_line_count()
+	string_list = "[right]"
 	for i in range(0, line_count):
 		string_list += str(i + 1) + "\n"
+	string_list += "\n[/right]"
 	self.set_text(string_list)
-	self.scroll_vertical = text_edit.scroll_vertical
-	
+	self.scroll_to_line(text_edit.scroll_vertical)
+
