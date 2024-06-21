572f9b114cda199bb9ed7665e1735cd3f6485ff0
JacobMundy <jmundy2003@gmail.com>
2024-06-21 16:45:31 -0400

    line counter removed
    replaced with Code edit node with built in counter

--- Main.gd
+++ Main.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var tab_container = $TabContainer
 @onready var add_tab_button = $AddTabButton 
-@onready var line_counter = $LineCounter
 @onready var word_counter = $WordCounter
 @onready var root = get_tree().root
 @onready var command_palette = $CommandPalette
@@ -30,23 +29,21 @@ func _input(event):
 
 func _on_window_resized():
 	var window_size = root.size
-	var tab_container_size = Vector2(window_size.x - 40, window_size.y - 30)  # Leave 30 pixels at the bottom for WordCounter
+	var tab_container_size = Vector2(window_size.x, window_size.y - 70)  # Leave 30 pixels at the bottom for WordCounter
 	var line_counter_size = Vector2(40, window_size.y - 30)
 	var word_counter_size = Vector2(window_size.x, 30)
 	var word_counter_position = Vector2(window_size.x - 400, window_size.y - 30)
 
 	tab_container.size = tab_container_size
-	line_counter.size = line_counter_size
 	word_counter.size = word_counter_size
 	word_counter.position = word_counter_position
 	
 
 func _on_AddTabButton_pressed():
 	# Logic to add a new tab to the TabContainer
-	var new_tab = TextEdit.new()
+	var new_tab = CodeEdit.new()
 	tab_number += 1
 	new_tab.name = "New "  + str(tab_number)
-	line_counter.set_new_text_edit(new_tab)
 	word_counter.set_new_text_edit(new_tab)
 	tab_container.add_child(new_tab)
 	tab_container.call_deferred("set_current_tab", tab_container.get_tab_count() - 1)  # Switch to the new tab
